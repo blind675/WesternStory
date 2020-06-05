0a51c034345cb7f5e35f9201cbf955a331375803
Bob <catalin.bora@gmail.com>
2020-06-05 14:39:54 +0300

    added player controller and UI

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	public static bool moveForward;
+	public static bool moveBackwards;
+	public static bool rotateCCW;
+	public static bool rotateCW;
+	public static bool fire;
+
+
+	public void StartMoveForward ()
+	{
+		moveForward = true;
+	}
+
+	public void StopMoveForward ()
+	{
+		moveForward = false;
+	}
+
+	public void StartMoveBackwards ()
+	{
+		moveBackwards = true;
+	}
+
+	public void StopMoveBackwards ()
+	{
+		moveBackwards = false;
+	}
+
+	public void StartRotateCW ()
+	{
+		rotateCW = true;
+	}
+
+	public void StopRotateCW ()
+	{
+		rotateCW = false;
+	}
+
+	public void StartRotateCCW ()
+	{
+		rotateCCW = true;
+	}
+
+	public void StopRotateCCW ()
+	{
+		rotateCCW = false;
+	}
+
+	public void StartFire ()
+	{
+		fire = true;
+	}
+
+	public void StopFire ()
+	{
+		fire = false;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Transform playerTransform;
+	public CharacterController characterController;
+
+	// Player settings
+	public float moveSpeed;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		GetInput ();
+	}
+
+	void GetInput ()
+	{
+		if (InputManager.moveForward || Input.GetKey (KeyCode.W)) {
+			characterController.Move (playerTransform.forward * moveSpeed * 0.1f);
+		}
+
+		if (InputManager.moveBackwards || Input.GetKey (KeyCode.S)) {
+			characterController.Move (playerTransform.forward * moveSpeed * -0.1f);
+		}
+
+		if (InputManager.rotateCW || Input.GetKey (KeyCode.D)) {
+			playerTransform.Rotate (playerTransform.up, 1);
+		}
+
+		if (InputManager.rotateCCW || Input.GetKey (KeyCode.A)) {
+			playerTransform.Rotate (playerTransform.up, -1);
+		}
+
+		if (InputManager.fire) {
+			//Debug.Log ("Move foreward");
+		}
+	}
+}
