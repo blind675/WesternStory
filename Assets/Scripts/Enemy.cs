512095fab6cede3b9fb1856823796d19c7d4b98b
Bob <catalin.bora@gmail.com>
2020-06-11 21:57:36 +0300

    created bear cave scene

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,6 +14,8 @@ public class Enemy : MonoBehaviour {
 	public AudioSource enemyHitFX;
 	public AudioSource enemyAttackFX;
 
+	public GameObject deathPrefab;
+
 	private bool isDying = false;
 	private bool enemyOnCoolDown = false;
 
@@ -45,35 +47,39 @@ public class Enemy : MonoBehaviour {
 			} else {
 				RaycastHit Shot;
 				if (Physics.Raycast (transform.position, transform.TransformDirection (Vector3.forward), out Shot)) {
-					targetDistance = Shot.distance;
 
-					if (targetDistance > activateDistance) {
-						enemyAnimator.SetBool ("isWalking", false);
-						enemyAnimator.SetBool ("isAttacking", false);
-						enemyAnimator.SetBool ("isDead", false);
-						enemyAnimator.SetBool ("isIdle", false);
-					} else {
-						if (targetDistance > attackDistance) {
-							transform.position = Vector3.MoveTowards (transform.position, Camera.main.transform.position, moveSpeed);
-							enemyAnimator.SetBool ("isWalking", true);
-							enemyAnimator.SetBool ("isAttacking", false);
-						} else {
-							// 
-							// attack animation
-							enemyAnimator.SetBool ("isAttacking", true);
+					if (Shot.transform.tag == "Player") {
+
+						targetDistance = Shot.distance;
+
+						if (targetDistance > activateDistance) {
 							enemyAnimator.SetBool ("isWalking", false);
+							enemyAnimator.SetBool ("isAttacking", false);
+							enemyAnimator.SetBool ("isDead", false);
+							enemyAnimator.SetBool ("isIdle", false);
+						} else {
+							if (targetDistance > attackDistance) {
+								transform.position = Vector3.MoveTowards (transform.position, Camera.main.transform.position, moveSpeed);
+								enemyAnimator.SetBool ("isWalking", true);
+								enemyAnimator.SetBool ("isAttacking", false);
+							} else {
+								// 
+								// attack animation
+								enemyAnimator.SetBool ("isAttacking", true);
+								enemyAnimator.SetBool ("isWalking", false);
 
-							if (!enemyOnCoolDown) {
-								// TODO: player hit indicator
-								// player hit sound
-								if (enemyAttackFX) enemyAttackFX.Play ();
-								// player life points decrease
-								PlayerHealth.SubstractHealth (attackDamage);
+								if (!enemyOnCoolDown) {
+									// TODO: player hit indicator
+									// player hit sound
+									if (enemyAttackFX) enemyAttackFX.Play ();
+									// player life points decrease
+									PlayerHealth.SubstractHealth (attackDamage);
 
-								enemyOnCoolDown = true;
-								StartCoroutine (EnemyCoolDownRoutine ());
+									enemyOnCoolDown = true;
+									StartCoroutine (EnemyCoolDownRoutine ());
+								}
+
 							}
-
 						}
 					}
 
@@ -87,7 +93,14 @@ public class Enemy : MonoBehaviour {
 	{
 		yield return new WaitForSeconds (2);
 
-		// TODO: spawn droped item ?
+		Vector3 deathPos = this.gameObject.transform.position;
+		deathPos.y = 0.25f;
+
+		if (deathPrefab) {
+			// spawn droped item 
+			GameObject prefabToSpawn = Instantiate (deathPrefab, deathPos, Quaternion.identity);
+		}
+
 		// TODO: increase score maybe ?
 		Destroy (gameObject);
 
