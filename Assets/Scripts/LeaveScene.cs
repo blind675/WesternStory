02a47dcfa34d2dd1e0f05bb95f34bfcd80e845b4
Bob <catalin.bora@gmail.com>
2020-08-14 12:47:49 +0300

    bug fix

--- Assets/Scripts/LeaveScene.cs
+++ Assets/Scripts/LeaveScene.cs
@@ -15,6 +15,13 @@ public class LeaveScene : MonoBehaviour {
 
 				storyController.GoToNextScene ();
 
+
+				InputManager.moveForward = false;
+				InputManager.moveBackwards = false;
+				InputManager.rotateCCW = false;
+				InputManager.rotateCW = false;
+				InputManager.fire = false;
+
 			} else {
 				infoText.SetActive (true);
 				infoText.GetComponent<Text> ().text = "Can't leave yet. Talk to " + StoryController.GetTriggerNPCName ();
